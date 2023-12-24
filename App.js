bb583280af863758790a2f4b468c901df25e59a4
rotichshadrack <rotichshadrack478@gmail.com>
2023-12-24 18:03:08 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App() {
+
+ 
+  console.log("App executed")
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style= {StyleSheet.buttonContainer}>
+        <Button onPress={this._onPressButton} title='Press Me'/>
+
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button 
+        onPress={this._onPressButton}
+        title='Press Me'
+        color="#841584"
+        />
+      </View>
+      <View style={styles.alternativeLayoutButtonContainer}>
+        <Button onPress={this._onPressButton} title='This looks great!'/>
+        <Button onPress={this._onPressButton} title='OK!' color="#841584"/>
+      </View>
+     
     </View>
+
   );
 }
 
@@ -16,5 +37,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer:{
+    margin:20,
+  },
+  alternativeLayoutButtonContainer:{
+    margin:20,
+    flexDirection:'row',
+    justifyContent:'space-between',
   },
 });
