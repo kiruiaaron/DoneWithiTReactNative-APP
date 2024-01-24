e21d869caec039cb82977d37359e9bc8964d09cc
kiruiaaron <kiruiaaron471@gmail.com>
2024-01-24 11:55:20 +0300

    changes

--- App.js
+++ App.js
@@ -1,57 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native';
-import { SafeAreaView } from 'react-native';
-import { ScrollView } from 'react-native';
+import React , {useState} from 'react';
+import { Pressable,StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App =()=>{
+  const [timesPressed, setTimePresssed] = useState(0);
 
- 
-  console.log("App executed")
-
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollview}>
-       <View style={styles.container}>
-      <View style= {StyleSheet.buttonContainer}>
-        <Button onPress={this._onPressButton} title='Press Me'/>
-
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button 
-        onPress={this._onPressButton}
-        title='Press Me'
-        color="#841584"
-        />
-      </View>
-      <View style={styles.alternativeLayoutButtonContainer}>
-        <Button onPress={this._onPressButton} title='This looks great!'/>
-        <Button onPress={this._onPressButton} title='OK!' color="#841584"/>
-      </View>
-     
-    </View>
-      </ScrollView>
-
-    </SafeAreaView>
-    
-
-  );
+  let textLog = '';
+  if (timesPressed>1){
+    textLog = timesPressed + 'x onPress';
+  }
+  else if (timesPressed  > 0){
+    textLog ='onPress';
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer:{
-    margin:20,
-  },
-  alternativeLayoutButtonContainer:{
-    margin:20,
-    flexDirection:'row',
-    justifyContent:'space-between',
-  },
-});
+return(
+  <View style ={StyleSheet.container}>
+    <Pressable
+    onPress={()=>{
+      setTimePresssed(current => current + 1);
+    }}
+    style={({pressed})=>[{
+      backgroundColor:pressed? 'rgb(210, 23, 255)': 'white',
+    },
+    styles.wrapperCustom,
+  ]}>
+    {({pressed})=>(
+      
+    )}
+  </Pressable>
+  </View>
+)
